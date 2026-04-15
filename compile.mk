00394387747532c4dac0318e0636dc6c41116725
Amrutha Kanhirathingal <Amrutha.Kanhirathingal@ibm.com>
2026-04-15 13:09:24 +0530

    Dynamically detect ant-contrib.jar path to support varying Ant installations across machines

--- compile.mk
+++ compile.mk
@@ -27,6 +27,7 @@ include moveDmp.mk
 #######################################
 # compile all tests under $(TEST_ROOT)
 #######################################
+
 ifneq ($(DYNAMIC_COMPILE), true)
 COMPILE_BUILD_LIST := ${BUILD_LIST}
 else
@@ -38,6 +39,28 @@ TEST_FLAG_PARAM := -DTEST_FLAG=$(TEST_FLAG)
 else
 TEST_FLAG_PARAM :=
 endif
+# Dynamically detect ant-contrib.jar location
+ANT_PATH := $(shell which ant 2>/dev/null)
+ANT_VERSION := $(shell ant -version 2>/dev/null | head -1)
+ANT_HOME := $(shell ant -diagnostics 2>/dev/null | grep "ant.home:" | head -1 | cut -d: -f2- | xargs)
+ANT_CONTRIB_JAR := $(shell find $(ANT_HOME)/lib $(HOME)/.ant/lib /usr/share /usr/local -name "ant-contrib*.jar" 2>/dev/null | head -1)
+
+# Display detection results
+$(info ========================================)
+$(info Ant executable: $(ANT_PATH))
+$(info Ant version: $(ANT_VERSION))
+$(info Ant home: $(ANT_HOME))
+$(info ant-contrib.jar: $(ANT_CONTRIB_JAR))
+$(info ========================================)
+
+# If ant-contrib.jar found, add it to ant classpath
+ifneq ($(ANT_CONTRIB_JAR),)
+ANT_CLASSPATH := -lib $(ANT_CONTRIB_JAR)
+else
+ANT_CLASSPATH :=
+$(warning WARNING: ant-contrib.jar not found. Build may fail.)
+endif
+
 COMPILE_CMD=ant -f scripts$(D)build_test.xml $(Q)-DTEST_ROOT=$(TEST_ROOT)$(Q) $(Q)-DBUILD_ROOT=$(BUILD_ROOT)$(Q) $(Q)-DJDK_VERSION=$(JDK_VERSION)$(Q) $(Q)-DJDK_IMPL=$(JDK_IMPL)$(Q) $(Q)-DJDK_VENDOR=$(JDK_VENDOR)$(Q) $(Q)-DJCL_VERSION=$(JCL_VERSION)$(Q) $(Q)-DBUILD_LIST=${COMPILE_BUILD_LIST}$(Q) $(Q)-DRESOURCES_DIR=${RESOURCES_DIR}$(Q) $(Q)-DSPEC=${SPEC}$(Q) $(Q)-DTEST_JDK_HOME=${TEST_JDK_HOME}$(Q) $(Q)-DJVM_VERSION=$(JVM_VERSION)$(Q) $(Q)-DLIB_DIR=$(LIB_DIR)$(Q) ${TEST_FLAG_PARAM}
 
 
